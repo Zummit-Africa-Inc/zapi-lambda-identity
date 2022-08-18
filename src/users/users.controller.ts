8e327318c481e3bfd4239da3c7ade6958d0fac7b
Ayinde John <lolaayinde@gmail.com>
2022-08-18 19:08:14 +0100

    provided endpoint for user login histories

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,35 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserInfoDto } from './dto/user-info.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { UserSignupDto } from 'src/auth/dto/user-signup.dto';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly userService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: UserSignupDto) {
-    return this.usersService.create(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Get('history/:userId')
+  @ApiOkResponse({
+    type: [UserInfoDto],
+    description: 'Get all histories',
+    isArray: true,
+  })
+  @ApiOperation({ description: 'Get user login histories' })
+  async getLoginHistory(@Param('userId') userId: string) {
+    return await this.userService.getLoginHistories(userId);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,28 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserInfoDto } from './dto/user-info.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserSignupDto } from '../auth/dto/user-signup.dto';
 import { User } from '../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ZaLaResponse } from 'src/common/helpers/response';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(userSignupDto: UserSignupDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepo: Repository<User>,
+  ) {}
 
-  findAll() {
-    return `This action returns all users`;
-  }
+  async getLoginHistories(id: string) {
+    const user = await this.usersRepo.findOne({ where: { id: id } });
+    if (!user) {
+      throw new NotFoundException(
+        ZaLaResponse.NotFoundRequest(
+          'Internal server error',
+          'user not found',
+          '404',
+        ),
+      );
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    return user.history;
   }
 }
