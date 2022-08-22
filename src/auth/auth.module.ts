a187545e7649ca89c7f3f9ba9fd63e2816629b65
Tijani Ayomide <ayomidetijani6@gmail.com>
2022-08-22 15:30:28 +0100

    added jwtService to module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
+import { jwtHelperService } from './jwt-helper.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, jwtHelperService]
 })
 export class AuthModule {}
