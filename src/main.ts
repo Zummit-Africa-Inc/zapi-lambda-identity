ce7debffc6e6e7e4e1398629bc4964c260ab89d0
Chimexx <mezie.cjo@gmail.com>
2022-09-20 16:23:27 +0100

    Removed node_env check to run swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
-  app.setGlobalPrefix('zapi-identity')
-  app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}))
+  app.enableCors();
+  app.setGlobalPrefix('zapi-identity');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  if(process.env.NODE_ENV !== 'production') {
-    const options = new DocumentBuilder()
-      .setTitle('ZA Lambda Identity Service')
-      .setDescription('Zummit Africa Lambda User Registration')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build()
-    
-    const document = SwaggerModule.createDocument(app, options)
-    SwaggerModule.setup('zl-id', app, document)
-  }
-  await app.listen(Number(process.env.NODE_PORT) ||3000);
+  const options = new DocumentBuilder()
+    .setTitle('ZA Lambda Identity Service')
+    .setDescription('Zummit Africa Lambda User Registration')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-doc', app, document);
+
+  await app.listen(Number(process.env.NODE_PORT) || 3000);
 }
 bootstrap();
