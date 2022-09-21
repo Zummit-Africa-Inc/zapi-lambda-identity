4cda454243c9ceb43ad65718b499c8d81a353019
Chimexx <mezie.cjo@gmail.com>
2022-09-21 11:00:04 +0100

    Added global prefix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('zapi-identity');
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const options = new DocumentBuilder()
