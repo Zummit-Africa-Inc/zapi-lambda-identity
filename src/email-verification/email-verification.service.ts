6d1d840b378e701adaf9205484f3f4a7ea8d6b07
Chimexx <mezie.cjo@gmail.com>
2023-03-17 15:41:52 +0100

    send mail with axios

--- src/email-verification/email-verification.service.ts
+++ src/email-verification/email-verification.service.ts
@@ -61,7 +61,17 @@ export class EmailVerificationService {
         text: emailBody,
       };
 
-      this.sendMail('mail', mailPayload);
+      const notificationUrl = `${this.configService.get<string>(
+        configConstant.baseUrls.notificationService,
+      )}/email/confirmation`;
+      console.log(notificationUrl);
+      this.httpService.axiosRef
+        .post(notificationUrl, mailPayload)
+        .catch((error) => {
+          console.log(error.message);
+        });
+
+      // this.sendMail('mail', mailPayload);
     } catch (error) {
       throw new BadRequestException(
         ZaLaResponse.BadRequest('Internal Server Error', error.message, '500'),
@@ -305,8 +315,12 @@ export class EmailVerificationService {
         text: text,
       };
 
-      await this.sendMail('mail', mailData);
+      // await this.sendMail('mail', mailData);
 
+      const notificationUrl = `${this.configService.get<string>(
+        configConstant.baseUrls.notificationService,
+      )}/email/confirmation`;
+      this.httpService.axiosRef.post(notificationUrl, mailData);
       return `Reset OTP successfully sent to ${userEmail}`;
     } catch (error) {
       throw new BadRequestException(
@@ -321,13 +335,13 @@ export class EmailVerificationService {
    * @param {object} body - body of the message
    */
 
-  async sendMail(pattern: string, payload: object): Promise<void> {
-    try {
-      this.n_client.emit(pattern, payload);
-    } catch (error) {
-      throw new BadRequestException(
-        ZaLaResponse.BadRequest('Internal Server error', error.message, '500'),
-      );
-    }
-  }
+  // async sendMail(pattern: string, payload: object): Promise<void> {
+  //   try {
+  //     this.n_client.emit(pattern, payload);
+  //   } catch (error) {
+  //     throw new BadRequestException(
+  //       ZaLaResponse.BadRequest('Internal Server error', error.message, '500'),
+  //     );
+  //   }
+  // }
 }
